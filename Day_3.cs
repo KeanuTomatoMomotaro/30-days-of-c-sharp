99c56c9b9783eeb0c2091ec6b7be39f3bcaf72f1
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2017-12-14 21:00:08 +0700

    Create Day_3.cs
    Apparently, c# does not support 0 <= x <= 2 style operations

---
+++ Day_3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        int N = Convert.ToInt32(Console.ReadLine());
+        if(N % 2 != 0) {
+            Console.WriteLine("Weird");
+        }else if((N % 2 == 0) && ((2 <= N) && (N <= 5))) {
+            Console.WriteLine("Not Weird");
+        }else if ((N % 2 == 0) && ((6 <= N) && (N <= 20))) {
+            Console.WriteLine("Weird");
+        }else if((N % 2 == 0) && (N>20)){
+            Console.WriteLine("Not Weird");
+        }
+    }
+}
